5e364b6cc4c029b0d8d700d7926747911d2728a9
Plenty Su <plenty.su@gmail.com>
2019-12-04 14:54:10 +0900

    Add KVUdf

---
+++ emulator/src/test/java/com/scalar/client/tool/emulator/KVUdf.java
@@ -0,0 +1,31 @@
+package com.scalar.client.tool.emulator;
+
+import com.scalar.database.api.Put;
+import com.scalar.database.io.Key;
+import com.scalar.database.io.TextValue;
+import com.scalar.ledger.database.MutableDatabase;
+import com.scalar.ledger.udf.Function;
+import java.util.Optional;
+import javax.json.JsonObject;
+
+class KVUdf extends Function {
+  @Override
+  public void invoke(
+      MutableDatabase database, JsonObject contractArgument, Optional<JsonObject> funtionArgument) {
+        System.out.println("hello world");
+
+    funtionArgument.ifPresent(
+        argument -> {
+          String k = argument.getString("key", "");
+          String v = argument.getString("value", "");
+          if (k == "") {
+            return;
+          }
+
+          Key key = new Key(new TextValue(k));
+          TextValue value = new TextValue(v);
+          Put put = (new Put(key)).withValue(value);
+          database.put(put);
+        });
+  }
+}
