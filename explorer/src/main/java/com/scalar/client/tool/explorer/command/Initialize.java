aae3242c43d064737564e0c6799ef656933998b7
scalar-boney <boney.jacob@scalar-labs.com>
2020-03-09 14:33:12 +0900

    Initialize command added with explorer - format change [skip ci]

--- explorer/src/main/java/com/scalar/client/tool/explorer/command/Initialize.java
+++ explorer/src/main/java/com/scalar/client/tool/explorer/command/Initialize.java
@@ -2,36 +2,32 @@ package com.scalar.client.tool.explorer.command;
 
 import picocli.CommandLine;
 
-import javax.json.JsonObject;
-
-/** This class defines the behaviour of <em>init</em> CLI command */
 @CommandLine.Command(name = "initialize", description = "Initialize the asset management application")
 public class Initialize implements Runnable {
-    @CommandLine.Option(
-            names = {"-f", "--file"},
-            description =
-                    "Specify an alternative client.properties file%n(default: client.properties)",
-            defaultValue = "client.properties")
-    private String file;
+  @CommandLine.Option(
+      names = {"-f", "--file"},
+      description = "Specify an alternative client.properties file%n(default: client.properties)",
+      defaultValue = "client.properties")
+  private String file;
 
-    @CommandLine.Option(
-            names = {"-h"},
-            usageHelp = true,
-            description = "display this help and exit")
-    private boolean help;
+  @CommandLine.Option(
+      names = {"-h"},
+      usageHelp = true,
+      description = "display this help and exit")
+  private boolean help;
 
-    @CommandLine.ParentCommand private Explorer parent;
+  @CommandLine.ParentCommand private Explorer parent;
 
-    @Override
-    public void run() {
-        Explorer.ExplorerExecutor executor =
-                explorer -> {
-                    try {
-                        explorer.initialize();
-                    } catch (Exception e) {
-                        System.err.println(e.getMessage());
-                    }
-                };
-        parent.execute(file, executor);
-    }
+  @Override
+  public void run() {
+    Explorer.ExplorerExecutor executor =
+        explorer -> {
+          try {
+            explorer.initialize();
+          } catch (Exception e) {
+            System.err.println(e.getMessage());
+          }
+    };
+    parent.execute(file, executor);
+  }
 }
